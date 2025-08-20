fbbb36318054e07bc37ed9b8b0479c3205b7bd01
eunji-0508 <cojia@naver.com>
2025-08-20 18:47:45 +0900

    feat: 댓글 생성 기능 구현

--- src/main/java/hello/newsfeed/comment/controller/CommentController.java
+++ src/main/java/hello/newsfeed/comment/controller/CommentController.java
@@ -1,4 +1,24 @@
 package hello.newsfeed.comment.controller;
 
+import hello.newsfeed.comment.dto.request.CommentCreateRequest;
+import hello.newsfeed.comment.dto.response.CommentCreateResponse;
+import hello.newsfeed.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+    // 댓글 생성 기능
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentCreateResponse> createComment(
+            @SessionAttribute(name = "LOGIN_USER") Long userId,
+            @PathVariable Long postId,
+            @RequestBody CommentCreateRequest commentRequest
+    ) {
+        return ResponseEntity.ok(commentService.createComment(userId, postId, commentRequest));
+    }
 }
--- src/main/java/hello/newsfeed/comment/service/CommentService.java
+++ src/main/java/hello/newsfeed/comment/service/CommentService.java
@@ -1,4 +1,50 @@
 package hello.newsfeed.comment.service;
 
+import hello.newsfeed.comment.dto.request.CommentCreateRequest;
+import hello.newsfeed.comment.dto.response.CommentCreateResponse;
+import hello.newsfeed.comment.entity.Comment;
+import hello.newsfeed.comment.repository.CommentRepository;
+import hello.newsfeed.post.entity.Post;
+import hello.newsfeed.post.repository.PostRepository;
+import hello.newsfeed.user.entity.User;
+import hello.newsfeed.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    // 댓글 생성
+    @Transactional
+    public CommentCreateResponse createComment(Long userId, Long postId, CommentCreateRequest commentRequest) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다.")
+        );
+
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다.")
+        );
+
+        Comment comment = new Comment(
+                commentRequest.getContent(),
+                user,
+                post
+        );
+
+        Comment savedComment = commentRepository.save(comment);
+
+        return new CommentCreateResponse(
+                post.getId(),
+                user.getId(),
+                savedComment.getId(),
+                savedComment.getContent(),
+                savedComment.getCreatedAt(),
+                savedComment.getModifiedAt()
+        );
+    }
 }
