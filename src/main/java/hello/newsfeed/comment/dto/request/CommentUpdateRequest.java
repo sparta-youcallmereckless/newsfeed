17263673b35a8373a42d8a55efd0fa8a80a83529
eunji-0508 <cojia@naver.com>
2025-08-21 13:06:33 +0900

    docs: CommentCreateRequest 주석 정리 및 개선

--- src/main/java/hello/newsfeed/comment/dto/request/CommentUpdateRequest.java
+++ src/main/java/hello/newsfeed/comment/dto/request/CommentUpdateRequest.java
@@ -3,8 +3,12 @@ package hello.newsfeed.comment.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
-@Getter
+// 댓글 수정용 RequestDto
+// RequestDto에는 final 키워드, 생성자 넣지 않음
+@Getter                                                 // getter 메서드를 자동으로 생성해주는 annotation
 public class CommentUpdateRequest {
+    // 해당 필드가 null, 빈 문자열, 공백이면 검증이 실패함
+    // message 속성: 검증 실패 시 반환할 메시지 내용을 적음
     @NotBlank(message = "내용을 입력해주시길 바랍니다.")
-    private String content;
+    private String content;                             // 댓글 내용
 }
