2564e21080db2f277a352edf0c552d4264278d18
HY-WG <mung94c@gmail.com>
2025-08-22 13:38:56 +0900

    refactor: follow  연관관계 추가

--- src/main/java/hello/newsfeed/follow/repository/FollowsRepository.java
+++ src/main/java/hello/newsfeed/follow/repository/FollowsRepository.java
@@ -1,8 +1,15 @@
 package hello.newsfeed.follow.repository;
 
 import hello.newsfeed.follow.entity.Follow;
+import hello.newsfeed.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FollowsRepository extends JpaRepository<Follow, Long> {
-    Follow findByFollowerIdAndFollowingId(Long followerId, Long followingId);
+    Follow findByFollowerAndFollowing(User follower, User following);
+
+    List<Follow> findAllByFollower(User follower);
+
+    List<Follow> findAllByFollowing(User following);
 }
